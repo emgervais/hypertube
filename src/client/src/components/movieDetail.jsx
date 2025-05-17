1d01f1d33c66e067801d3d6109a265fba16c541b
emile <guaitanverish2@outlook.com>
2025-05-18 00:36:01 +0200

    iproved

--- src/client/src/components/movieDetail.jsx
+++ src/client/src/components/movieDetail.jsx
@@ -9,6 +9,7 @@ export default function MovieDetail() {
   const mediaSourceRef = useRef(null);
   const mp4boxFile = useRef(MP4Box.createFile());
   const nextSegmentIndex = useRef(0);
+  const pumpingFlag = useRef(false);
   const segmentSize = 5;
 
   const fetchSegment = async (index) => {
@@ -31,25 +32,40 @@ export default function MovieDetail() {
     const mediaSource = new MediaSource();
     mediaSourceRef.current = mediaSource;
     videoRef.current.src = URL.createObjectURL(mediaSource);
-    setInterval(() => {
-      if(!sourceBufferRef.current.buffered.length) return;
-      const start = sourceBufferRef.current.buffered.start(0);
-      const end = videoRef.current.currentTime - 30;
-      if(sourceBufferRef.current.updating || start >= end)
-        return;
-      sourceBufferRef.current.remove(start, end);
-    }, 1000);
+    videoRef.current.addEventListener('seeking', (event) => {
+      nextSegmentIndex.current = Math.floor(videoRef.current.currentTime / segmentSize) - 1;
+      console.log('new segment: ', nextSegmentIndex.current)
+      pumpNextSegment();
+    });
+    //init
     mediaSource.addEventListener('sourceopen', async () => {
-      const initBuf = await fetchSegment(0);
       const res = await fetch(`http://127.0.0.1:8080/stream/manifest?id=${location.state.movie.id}`);
       const manifest = await res.json();
+      const initBuf = await fetchSegment(0);
       mediaSourceRef.current.duration = manifest.length;
       initBuf.fileStart = 0;
       mp4boxFile.current.onReady = onMp4Ready;
       mp4boxFile.current.appendBuffer(initBuf);
-      mp4boxFile.current.flush();
     });
-  };
+    //clean
+    setInterval(() => {
+      for(let i = 0; i < sourceBufferRef.current.buffered.length; i++) {
+        const start = sourceBufferRef.current.buffered.start(i);
+        const playback = Math.floor(videoRef.current.currentTime);
+        let end = 0;
+        if(start > playback + 50) {
+          end = sourceBufferRef.current.buffered.end(i);
+        } else if (start < playback - 10) {
+          end = playback - 10;
+        } 
+        else
+          continue;
+        if(sourceBufferRef.current.updating || start >= end || end - start < 10) return;
+        console.log(`playback: ${playback} clean ${start}-${end}`);
+        sourceBufferRef.current.remove(start, end);
+      }
+    }, 5000);
+ };
 
   const onMp4Ready = (info) => {
     mp4boxFile.current.onReady = null;
@@ -60,25 +76,29 @@ export default function MovieDetail() {
     const mime = `video/mp4; codecs="${codecs}"`;
     const sb = mediaSourceRef.current.addSourceBuffer(mime);
     sourceBufferRef.current = sb;
-    // sourceBufferRef.current.mode = 'sequence';
 
     nextSegmentIndex.current = 0;
     sb.addEventListener('updateend', pumpNextSegment);
     pumpNextSegment();
   };
   const pumpNextSegment = async () => {
-    const buffered = sourceBufferRef.current.buffered;
-    let totalBuffered = 0;
-    for (let i = 0; i < buffered.length; i++) {
-        totalBuffered += buffered.end(i) - buffered.start(i);
-    }
-    if(totalBuffered > 120) return;
-    const currIndex = Math.floor(videoRef.current.currentTime / segmentSize);
-    nextSegmentIndex.current = currIndex > nextSegmentIndex.current + 11? currIndex - 10 : nextSegmentIndex.current;
+    if(pumpingFlag.current) return;
+    pumpingFlag.current = true;
+    const currentPiece = Math.floor(videoRef.current.currentTime / segmentSize);
+    if(nextSegmentIndex.current - currentPiece >= 10) {
+      console.log(`playback ${videoRef.current.currentTime} `+'total buffeer:', nextSegmentIndex.current - currentPiece);
+      pumpingFlag.current = false;
+      return;
+    } 
     try {
       const buf = await fetchSegment(nextSegmentIndex.current);
+      if(sourceBufferRef.current.updating) {
+        pumpingFlag.current = false;
+        return;
+      }
       sourceBufferRef.current.timestampOffset = nextSegmentIndex.current++ * segmentSize;
       sourceBufferRef.current.appendBuffer(buf);
+      pumpingFlag.current = false;
     } catch(e) {
       console.log(e)
       mediaSourceRef.current.endOfStream();
